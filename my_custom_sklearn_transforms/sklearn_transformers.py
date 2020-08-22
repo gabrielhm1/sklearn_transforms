66edd26ae861070d793a21367cbcd9913a20c5d8
gabrielhm1 <69736891+gabrielhm1@users.noreply.github.com>
2020-08-22 02:55:56 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,23 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+    
+class PersFill(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
+         
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        
+        data = X.copy()
+        target = self.columns[0]
+        cls = data[target].unique()
+        for x in cls:
+            for col in data.columns:
+                if col != target:
+                    data.loc[( (data[target]==x) & (data[col].isnull()) ),col] = data[col].loc[(data[target]==x)].mean()
+        return data 
